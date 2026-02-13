db034b0fc276c7098505079538eacb2dfb1b95ff
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:31:42 +0100

    chore: update plugin categories (#46)

--- src/main/java/io/kestra/plugin/typesense/package-info.java
+++ src/main/java/io/kestra/plugin/typesense/package-info.java
@@ -1,7 +1,7 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Typesense collection database.\n" +
         "Typesense is a RESTful search API. It aims to be a ready-to-go solution for everyone who wants a fast and relevant search experience for their end-users ",
-    categories = { PluginSubGroup.PluginCategory.DATABASE }
+        categories = { PluginSubGroup.PluginCategory.DATA }
 )
 package io.kestra.plugin.typesense;
 
