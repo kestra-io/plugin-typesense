b2ac837fcc6b9a1f0cf46b48204711fcc561b5b0
Pradumna Saraf <pradumnasaraf@gmail.com>
2026-03-19 14:02:03 +0530

    feat: add RunnerTest and sanity checks

--- src/test/java/io/kestra/plugin/typesense/TypesenseRunnerTest.java
+++ src/test/java/io/kestra/plugin/typesense/TypesenseRunnerTest.java
@@ -1,61 +1,22 @@
 package io.kestra.plugin.typesense;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.TimeoutException;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import io.kestra.core.junit.annotations.ExecuteFlow;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
-import io.kestra.core.queues.QueueException;
-import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.TestRunner;
-import io.kestra.core.runners.TestRunnerUtils;
-import io.kestra.core.tenant.TenantService;
-import io.kestra.plugin.typesense.typesense.TypesenseContainer;
-
-import jakarta.inject.Inject;
+import io.kestra.core.models.flows.State;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
-/**
- * This test will load all flow located in `src/test/resources/flows/`
- * and will run an in-memory runner to be able to test a full flow. There is also a
- * configuration file in `src/test/resources/application.yml` that is only for the full runner
- * test to configure in-memory runner.
- */
-@KestraTest
-class TypesenseRunnerTest extends TypesenseContainer {
-    @Inject
-    protected TestRunner runner;
-
-    @Inject
-    protected TestRunnerUtils runnerUtils;
-
-    @Inject
-    protected LocalFlowRepositoryLoader repositoryLoader;
-
-    @BeforeEach
-    protected void init() throws IOException, URISyntaxException {
-        repositoryLoader.load(Objects.requireNonNull(TypesenseRunnerTest.class.getClassLoader().getResource("flows")));
-        this.runner.run();
-    }
+@KestraTest(startRunner = true)
+class TypesenseRunnerTest {
 
     @Test
-    void flow() throws TimeoutException, QueueException {
-        Execution execution = runnerUtils.runOne(TenantService.MAIN_TENANT, "io.kestra.plugin.typesense", "typesense");
-
-        assertThat(execution.getTaskRunList(), hasSize(2));
-        assertThat(execution.getTaskRunList().get(0).getOutputs().size(), is(0));
-        assertThat(
-            execution.getTaskRunList().get(1).getOutputs().get("document"),
-            is(Map.of("countryName", "France", "capital", "Paris", "gdp", 123456, "id", "0"))
-        );
+    @ExecuteFlow("sanity-checks/all_typesense.yaml")
+    void all_typesense(Execution execution) {
+        assertThat(execution.getTaskRunList(), hasSize(14));
+        assertThat(execution.getState().getCurrent(), is(State.Type.SUCCESS));
     }
 }
